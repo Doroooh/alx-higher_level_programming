71d0b594a0c41cd95d88a2a194e5c5333b70aec4
Waithira <dorotttyyy@gmail.com>
2024-03-11 15:21:08 -0700

    python

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,13 +1,2 @@
-#!/usr/bin/python3
-import sys
-import os
-import compileall
-
-pyfile = os.environ.get('PYFILE')
-if not pyfile:
-    print("Environment variable PYFILE not set.")
-    sys.exit(1)
-
-pyfilec = f"{pyfile}c"
-compileall.compile_file(pyfile, pyfilec)
-print(f"Compiling {pyfile}...")
+def magic_calculation(a, b):
+    return 98 + a**b
