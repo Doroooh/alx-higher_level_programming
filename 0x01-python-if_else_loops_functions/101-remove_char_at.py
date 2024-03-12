1c6ab29724962919f3784242f9bf076aaca24497
Waithira <dorotttyyy@gmail.com>
2024-03-12 16:32:52 -0700

    tasks python

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#creating the string copy without character that is in position n
+def remove_char_at(string, n):
+    if 0 > n:
+        return string
+    return string[:n] + string[n+1:]
+
