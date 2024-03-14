5ca419e8617711eeb737d8dab67f9b7636dc3142
Waithira <dorotttyyy@gmail.com>
2024-03-14 11:46:34 -0700

    addidtion python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+if_name_ == "_main_":
 # Importing the add function from add_0.py
-from add_0 import add
-
-# Define variables a and b
+from add_0 import add 
+# Defining my two variables a and b
 a = 1
 b = 2
 
-# Call the add function and print the result
+# Calling my function to get the expected return for 1 and 2 using the print function
 print("{} + {} = {}".format(a, b, add(a, b)))
