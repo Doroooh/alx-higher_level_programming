4ffdd8fbdc5e02ed2d3c11643735e992f72a1251
Waithira <dorotttyyy@gmail.com>
2024-03-14 12:57:04 -0700

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+import dis
+
+def print_names(filename):
+    with open(filename, 'rb') as f:
+        code = f.read()
+    dis.dis(code)
+    print_names("hidden_4.pyc")
+
