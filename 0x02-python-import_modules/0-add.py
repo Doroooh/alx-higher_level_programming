e8206c6de15e4445a02056cdb715bb7be343ce54
Waithira <dorotttyyy@gmail.com>
2024-03-14 11:37:01 -0700

    tasks

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Importing the add function from add_0.py
+from add_0 import add
+
+# Define variables a and b
+a = 1
+b = 2
+
+# Call the add function and print the result
+print("{} + {} = {}".format(a, b, add(a, b)))
