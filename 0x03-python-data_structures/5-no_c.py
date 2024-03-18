96409970231de196992826d5cda8ab8047b5320a
Waithira <dorotttyyy@gmail.com>
2024-03-18 13:56:22 -0700

    datas

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
     new = ''
-    for i in range(len(my_string)):
-        if my_string[i].lower() != 'c':
+    for k in range(len(my_string)):
+        if my_string[k].lower() != 'c':
             new += my_string[i]
     return new
