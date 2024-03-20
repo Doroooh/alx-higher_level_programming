a221075fede5d81adec0c6a00c099fd28107df78
Waithira <dorotttyyy@gmail.com>
2024-03-20 15:51:54 -0700

    tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[k**2 for k  in row] for row in matrix]
+    new_matrix = [[k ** 2 for k  in row] for row in matrix]
     return new_matrix
