c3c1fee6a6e0af2a39ce6ace0b9e63b449ff337e
Waithira <dorotttyyy@gmail.com>
2024-04-02 13:46:35 -0700

    printing division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
+    """
+    The code will safely perform division and print result.
+
+    Args:
+        a (float or int): numerator.
+        b (float or int): denominator.
+
+    Returns:
+        float or None: Quotient if the division is successful, otherwise None.
+    """
+
     try:
         quotient = a / b
     except ZeroDivisionError:
