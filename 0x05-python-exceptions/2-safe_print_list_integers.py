d2bfea79f46bb0b4fac1375aa820775438e4d077
Waithira <dorotttyyy@gmail.com>
2024-04-02 13:41:03 -0700

    python exception tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+
+def safe_print_list_integers(my_lst=None, x=0):
+    """
+    Safely printing the first 'x' integers from a list.
+
+    Args:
+        my_lst (list): The list to print integers from.
+        x (int): The number of integers to print.
+
+    Returns:
+        int: The number of integers actually printed.
+    """
+    if my_lst is None:
+        my_lst = []
+    
     count = 0
+
     try:
-        for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end="")
+        for item in my_lst[:x]:
+            if isinstance(item, int):
+                print("{:d}".format(item), end="")
                 count += 1
-    except ValueError and TypeError:
-        return
+    except (ValueError, TypeError, IndexError):
+        pass
     else:
-        print()
+        print()  # Adding a newline after the integers are printed.
         return count
