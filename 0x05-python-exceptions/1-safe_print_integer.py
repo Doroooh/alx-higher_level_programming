9dc0c1ec37035e0aff0cab0f545a0f26e89bdbd7
Waithira <dorotttyyy@gmail.com>
2024-04-02 13:34:34 -0700

    python exception tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+
+def safe_print_integer(myValue):
+    """
+    Safely prints integer values.
+
+    Args:
+        myValue (int): Integer value for printing.
+
+    Returns:
+        bool: if printing is successful True, False otherwise.
+    """
     try:
-        print("{:d}".format(value))
-    except:
-        return False
-    else:
+        print("{:d}".format(myValue))
         return True
+    except (ValueError, TypeError):
+        return False
