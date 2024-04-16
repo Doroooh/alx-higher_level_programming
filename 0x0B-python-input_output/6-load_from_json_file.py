007496c253bc138268045f8582ac8eaab88b5b62
Waithira <dorotttyyy@gmail.com>
2024-04-16 14:08:50 -0700

    input and output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-
+"""save_to_json_file module.
+this contains a function to create an Object from the “JSON file”.
+"""
 import json
 
 
 def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """Creating an Object from a “JSON file”."""
+    with open(filename, 'r') as f:
         return json.load(f)
