43b92a69ae8f06ad8e954606182fd769398a1c6b
Waithira <dorotttyyy@gmail.com>
2024-07-05 13:32:49 -0700

    python network 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+"""
+This script takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import sys
 import requests
 
-
-if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+def search_user(letter):
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {'q': letter}
+    
+    response = requests.post(url, data=payload)
+    
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        json_response = response.json()
+        
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("No result")
+    
     except ValueError:
         print("Not a valid JSON")
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    search_user(letter)
