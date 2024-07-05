7f2e6fcb2f977ddeab790fe0451b526fe02beac7
Waithira <dorotttyyy@gmail.com>
2024-07-05 13:20:11 -0700

    python network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""This script will fetch https://intranet.hbtn.io/status"""
+
 import urllib.request
 
-
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+def fetch_status(url):
+    request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+if __name__ == "__main__":
+    fetch_status("https://intranet.hbtn.io/status")
