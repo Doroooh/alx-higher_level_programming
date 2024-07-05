ec5e2550e5bed4166853e240e5b13174fc226de1
Waithira <dorotttyyy@gmail.com>
2024-07-05 14:25:48 -0700

    python network 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""
+Sending a POST request to a given URL with a given email
+Display the body of the response.
 """
 import sys
 import requests
@@ -10,7 +9,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    firstvalue = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    req = requests.post(url, datavalue=firstvalue)
+    print(req.text)
