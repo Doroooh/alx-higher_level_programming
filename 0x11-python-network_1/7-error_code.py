685f174380ece32b6a66cdd5cd36c7e6d5fcabf4
Waithira <dorotttyyy@gmail.com>
2024-07-05 14:30:08 -0700

    python network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""Sending the request to a url then  printing responses"""
-
-from requests import get
-from sys import argv
-
-
-def request_header_property(url: str) -> str:
-"""sending the requests to URL, and getting responses
-   , also handles exceptions  Args
-   url (str) is the URL to query
 """
-    response = get(url)
-    if int(response.status_code) >= 400:
-        return ("Error code: {}".format(response.status_code))
-    return response.text
+Script will take URL, send a request to the URL and display body of the response.
+
+"""
+from sys import argv
+import requests
+
 if __name__ == "__main__":
-    print(request_header_property(argv[1]))
+    url = argv[1]
+    addreqst = requests.get(url)
+
+    if addreqst.status_code >= 400:
+        print("Error code: {}".format(addreqst.status_code))
+    else:
+        print(addreqst.text)
