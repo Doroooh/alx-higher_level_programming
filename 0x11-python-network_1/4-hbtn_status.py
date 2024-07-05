e3ae69d6b74e13c252242f745f6e84e6a5532ca1
Waithira <dorotttyyy@gmail.com>
2024-07-05 14:20:16 -0700

    python network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import requests
 
+"""
+the script to get https://alx-intranet.hbtn.io/status with
+the request, printing response to stdout
+"""
+
+from requests import get
+
+
+def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
+"""
+Sending  GET request to  url, printing the response
+"""
+    resrq = get(url)
+    print("Body response:")
+    print("\t- type: {}".format(str(resrq).__class__))
+    print("\t- content: {}".format(resrq.text))
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    get_alx_intranet()
