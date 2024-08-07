0cdea17696c1866d24820edc5310eea5c6b533ff
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-07 23:40:34 +0300

    Web JSquery

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,17 @@
-// script that toggles the class of the <header> element when the user
-// clicks on the tag DIV#toggle_header
-const $ = window.$;
-$('#toggle_header').bind('click', function () {
-  $('header').toggleClass('green red');
+// Toggle the class of the <header> tag DIV#toggle_header
+
+$(document).ready(function () {
+  $('DIV#toggle_header').click(function () {
+    const the_class = ['green', 'red'];
+    const new_class = $('header').attr('class');
+
+    // alternate between green and red with for loop
+    for (let r = 0; r < classes.length; r++) {
+      if (new_class === the_class[r]) {
+        $('header').removeClass(the_class[r]);
+        $('header').addClass(the_class[(r + 1) % the_class.length]);
+        break;
+      }
+    }
+  });
 });
