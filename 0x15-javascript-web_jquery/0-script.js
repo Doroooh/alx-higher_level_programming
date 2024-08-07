67e1a358e0d21ad78d33846c003b1e72bf3f1f6d
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-07 23:52:37 +0300

    Web JSquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('header').style.color = '#FF0000';
-});
+// Update <header> text color to red, use document.querySelector
+
+document.querySelector('header').style.color = '#FF0000';
